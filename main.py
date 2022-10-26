b4a3b5c99c2385cef1d67e48d3a0c29962127949
karabaevakbar <akbarkarabaev303@gmail.com>
2022-10-26 17:56:36 +0600

    Second Commit

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from aiogram.utils import executor
 from config import bot, dp
 import logging
-from handlers import admin, callback, extra, client, fsmAdminMentor
+from handlers import admin, callback, extra, client, notifications
+from Database.bot_db import sql_create
+import asyncio
+
+async def on_startup(_):
+    sql_create()
+asyncio.create_task(notifications.scheduler())
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
-
+notifications.register_handlers_notification(dp)
 extra.register_handlers_extra(dp)
-fsmAdminMentor.register_handlers_fsm(dp)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
